0dc20f9f2ef4edf5322244909a3899002b98cfa6
Nitin2822 <nitinkoyyana22@gmail.com>
2026-02-05 19:17:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-// This is the boilerplate code given for you
-// You can modify this code
-// Product data
-const products = [
-  { id: 1, name: "Product 1", price: 10 },
-  { id: 2, name: "Product 2", price: 20 },
-  { id: 3, name: "Product 3", price: 30 },
-  { id: 4, name: "Product 4", price: 40 },
-  { id: 5, name: "Product 5", price: 50 },
-];
-
-// DOM elements
 const productList = document.getElementById("product-list");
+const cartList = document.getElementById("cart-list");
+const clearCartBtn = document.getElementById("clear-cart-btn");
+let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
 
-// Render product list
 function renderProducts() {
   products.forEach((product) => {
     const li = document.createElement("li");
@@ -21,18 +11,43 @@ function renderProducts() {
   });
 }
 
-// Render cart list
-function renderCart() {}
+function renderCart() {
+  cartList.innerHTML = "";
+  cart.forEach((item) => {
+    const li = document.createElement("li");
+    li.textContent = `${item.name} - $${item.price}`;
+    cartList.appendChild(li);
+  });
 
-// Add item to cart
-function addToCart(productId) {}
+ 
+  sessionStorage.setItem("cart", JSON.stringify(cart));
+}
 
-// Remove item from cart
-function removeFromCart(productId) {}
+function addToCart(productId) {
+  const productToAdd = products.find((p) => p.id === productId);
+  
+  if (productToAdd) {
+    cart.push(productToAdd);
+    renderCart();
+  }
+}
 
-// Clear cart
-function clearCart() {}
 
-// Initial render
+function clearCart() {
+  cart = []; 
+  renderCart(); 
+}
+
+
+productList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("add-to-cart-btn")) {
+    const productId = parseInt(e.target.getAttribute("data-id"));
+    addToCart(productId);
+  }
+});
+
+clearCartBtn.addEventListener("click", clearCart);
+
+
 renderProducts();
 renderCart();
